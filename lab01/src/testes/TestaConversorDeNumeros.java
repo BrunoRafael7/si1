a102f152f9d9e50f241d9ab94b6e71bdc847ade5
BrunoRafael7 <bruno.rafha@gmail.com>
2013-11-08 11:15:35 -0300

    testando entrada simples

---
+++ lab01/src/testes/TestaConversorDeNumeros.java
@@ -0,0 +1,20 @@
+package testes;
+
+import org.junit.*;
+
+public class TestaConversorDeNumeros {
+	
+	private ConversorDeNumeros conversor;
+	
+	@Before
+	public void init(){
+		conversor = new ConversorDeNumeros();
+	}
+	
+	@Test
+	public void entradaSimples(){
+		assertEquals(conversor.convert(10), "dez");
+		assertEquals(conversor.convert(1), "um");
+	}
+
+}
