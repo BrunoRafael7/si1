4ef429425ab1ba178a97ad9b955cf64c53669558
BrunoRafael7 <bruno.rafha@gmail.com>
2013-11-08 15:36:13 -0300

    testando e implementando conversao de nomenclatura dos numeros

--- lab01/src/conversordenumeros/ConversorDeNumeros.java
+++ lab01/src/conversordenumeros/ConversorDeNumeros.java
@@ -3,13 +3,65 @@ package conversordenumeros;
 public class ConversorDeNumeros {
 	
 private String [] nomesDasUnidades;
+private String [] nomesDosvaloresEntreDezEvinte;
+private String [] nomesDasDezenas;
+private String [] nomesDeMilhares;
+
 	
 	public ConversorDeNumeros(){
-		nomesDasUnidades = new String[21];
+		nomesDasUnidades = new String[10];
+		nomesDosvaloresEntreDezEvinte = new String[10];
+		nomesDasDezenas = new String[10];
 		preencheArrayDeNomes();
 	}
 	
 	private void preencheArrayDeNomes() {
+		preencheArrayDeUnidades();
+		preencheArrayDeValoresEntreDezEvinte();
+		preencheArrayDeDezenas();
+		preencheArrayDeCentenas();
+		preencheArrayDeMilhares();
+	}
+
+	private void preencheArrayDeMilhares() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void preencheArrayDeCentenas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void preencheArrayDeDezenas() {
+		nomesDasDezenas[0] = "";
+		nomesDasDezenas[1] = "dez";
+		nomesDasDezenas[2] = "vinte";
+		nomesDasDezenas[3] = "trinta";
+		nomesDasDezenas[4] = "quarenta";
+		nomesDasDezenas[5] = "cinquenta";
+		nomesDasDezenas[6] = "sessenta";
+		nomesDasDezenas[7] = "setenta";
+		nomesDasDezenas[8] = "oitenta";
+		nomesDasDezenas[9] = "noventa";
+		
+	}
+
+	private void preencheArrayDeValoresEntreDezEvinte() {
+		nomesDosvaloresEntreDezEvinte[0] = "onze";
+		nomesDosvaloresEntreDezEvinte[1] = "onze";
+		nomesDosvaloresEntreDezEvinte[2] = "doze";
+		nomesDosvaloresEntreDezEvinte[3] = "treze";
+		nomesDosvaloresEntreDezEvinte[4] = "quatorze";
+		nomesDosvaloresEntreDezEvinte[5] = "quinze";
+		nomesDosvaloresEntreDezEvinte[6] = "dezesseis";
+		nomesDosvaloresEntreDezEvinte[7] = "dezessete";
+		nomesDosvaloresEntreDezEvinte[8] = "dezoito";
+		nomesDosvaloresEntreDezEvinte[9] = "dezenove";
+		
+	}
+
+	private void preencheArrayDeUnidades() {
 		nomesDasUnidades[0] = "zero";
 		nomesDasUnidades[1] = "um";
 		nomesDasUnidades[2] = "dois";
@@ -20,17 +72,6 @@ private String [] nomesDasUnidades;
 		nomesDasUnidades[7] = "sete";
 		nomesDasUnidades[8] = "oito";
 		nomesDasUnidades[9] = "nove";
-		nomesDasUnidades[10] = "dez";
-		nomesDasUnidades[11] = "onze";
-		nomesDasUnidades[12] = "doze";
-		nomesDasUnidades[13] = "treze";
-		nomesDasUnidades[14] = "quatorze";
-		nomesDasUnidades[15] = "quinze";
-		nomesDasUnidades[16] = "dezesseis";
-		nomesDasUnidades[17] = "dezessete";
-		nomesDasUnidades[18] = "dezoito";
-		nomesDasUnidades[19] = "dezenove";
-		nomesDasUnidades[20] = "vinte";
 		
 	}
 
